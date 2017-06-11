b2a9258183619fa9a0af386d96e785a1fdaf33fd
Alpha Chen <alpha.chen@gmail.com>
2017-06-11 08:14:31 -0700

    Handle future emojis

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-#![feature(try_from)]
 #![recursion_limit = "1024"]
 
 extern crate rayon;
@@ -14,7 +13,6 @@ extern crate error_chain;
 extern crate serde_derive;
 
 use std::{env, fs};
-use std::convert::TryFrom;
 use std::io::prelude::*;
 use std::path::Path;
 use std::path::PathBuf;
@@ -22,7 +20,6 @@ use std::path::PathBuf;
 use rayon::prelude::*;
 use reqwest::Url;
 use select::document::Document;
-use select::node::Node;
 use select::predicate::{Class, Name, Predicate};
 
 mod errors {
@@ -96,13 +93,25 @@ impl Workflow {
             .chain_err(|| "Unable to parse search results")?;
 
         doc.find(Class("search-results").descendant(Name("h2").descendant(Name("a"))))
-            .map(SearchResult::try_from)
+            .flat_map(|node| {
+                node.find(Class("emoji")).next().map(|elem| (node, elem.text()))
+            })
+            .map(|(node, emoji)| {
+                let href = node.attr("href")
+                    .ok_or_else(|| "Unable to get href")?
+                    .to_string();
+                let mut children = node.children();
+                let text = children
+                    .nth(1)
+                    .ok_or_else(|| "Unable to get text")?
+                    .text();
+                Ok(SearchResult { href, emoji, text })
+            })
             .collect()
     }
 
     fn items(&self, results: Vec<SearchResult>) -> Result<Vec<Item>> {
         let mut items = vec![];
-        let base_url = Url::parse("https://emojipedia.org").unwrap();
         results
             .par_iter()
             .map(|ref search_result| {
@@ -110,31 +119,7 @@ impl Workflow {
                 let emoji = &search_result.emoji;
 
                 let file_name = format!("{}.png", href.trim_matches('/'));
-                let cache_path = self.cache(&file_name, || {
-                        let url = base_url.join(&href).unwrap();
-                        let res = reqwest::get(url).chain_err(|| "Unable to fetch emoji")?;
-
-                        let doc = Document::from_read(res)
-                            .chain_err(|| "Unable to parse emoji")?;
-                        let vendor_image = doc.find(Class("vendor-image"))
-                            .next()
-                            .ok_or_else(|| "Unable to find emoji image")?;
-                        let img = vendor_image
-                            .find(Name("img"))
-                            .next()
-                            .ok_or_else(|| "Unable to find emoji image")?;
-                        let src = img.attr("src")
-                            .ok_or_else(|| "Unable to find emoji image")?;
-
-                        let url = Url::parse(src)
-                            .chain_err(|| "Unable to find emoji image")?;
-                        let mut res = reqwest::get(url)
-                            .chain_err(|| "Unable to download emoji image")?;
-                        let mut image = vec![];
-                        res.read_to_end(&mut image)
-                            .chain_err(|| "Unable to save emoji image")?;
-                        Ok(image)
-                    })?;
+                let cache_path = self.cache(&file_name, || self.download_emoji_image(href))?;
 
                 let uid = Some(emoji.clone());
                 let title = search_result.text.clone();
@@ -166,33 +151,39 @@ impl Workflow {
         }
         Ok(file_path)
     }
+
+    fn download_emoji_image(&self, href: &str) -> Result<Vec<u8>> {
+        let base_url = Url::parse("https://emojipedia.org").unwrap();
+        let url = base_url.join(&href).unwrap();
+        let res = reqwest::get(url).chain_err(|| "Unable to fetch emoji")?;
+
+        let doc = Document::from_read(res)
+            .chain_err(|| "Unable to parse emoji")?;
+        let vendor_image = doc.find(Class("vendor-image"))
+            .next()
+            .ok_or_else(|| "Unable to find emoji image")?;
+        let img = vendor_image
+            .find(Name("img"))
+            .next()
+            .ok_or_else(|| "Unable to find emoji image")?;
+        let src = img.attr("src")
+            .ok_or_else(|| "Unable to find emoji image")?;
+
+        let url = Url::parse(src)
+            .chain_err(|| "Unable to find emoji image")?;
+        let mut res = reqwest::get(url)
+            .chain_err(|| "Unable to download emoji image")?;
+        let mut image = vec![];
+        res.read_to_end(&mut image)
+            .chain_err(|| "Unable to save emoji image")?;
+        Ok(image)
+    }
 }
 
 struct SearchResult {
     href: String,
     emoji: String,
     text: String,
-}
-
-impl<'a> TryFrom<Node<'a>> for SearchResult {
-    type Error = Error;
-
-    fn try_from(node: Node) -> Result<Self> {
-        let href = node.attr("href")
-            .ok_or_else(|| "Unable to get href")?
-            .to_string();
-        let mut children = node.children();
-        let emoji = children
-            .next()
-            .ok_or_else(|| "Unable to get emoji")?
-            .text();
-        let text = children
-            .next()
-            .ok_or_else(|| "Unable to get text")?
-            .text();
-
-        Ok(SearchResult { href, emoji, text })
-    }
 }
 
 #[derive(Serialize)]

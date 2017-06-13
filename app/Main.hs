7fc77f67c5f7955b1f2123191be6eec6abaebddf
Alpha Chen <alpha.chen@gmail.com>
2017-06-13 12:32:35 -0700

    Make an HTTP request

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,22 @@
+{-# LANGUAGE OverloadedStrings    #-}
+
 module Main where
 
+import Control.Exception (throwIO)
 import System.Environment
+
+import Network.HTTP.Req
+
+instance MonadHttp IO where
+  handleHttpException = throwIO
 
 main :: IO ()
 main = do
   args <- getArgs
   let query = head args
-  putStrLn query
+  res <- req GET
+    (https "emojipedia.org" /: "search")
+    NoReqBody
+    bsResponse
+    ("q" =: query)
+  print (responseBody res)
